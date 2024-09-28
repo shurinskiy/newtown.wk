74ae1ca53b5896b4e38e51f4d25ec3ecb1380b21
Alexandr Tumanov <shurinskiy2@gmail.com>
2024-09-28 15:28:53 +0500

    desktop ready

--- src/blocks/team/team.js
+++ src/blocks/team/team.js
@@ -1,4 +1,33 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+
 (() => {
 
+	document.querySelectorAll('.team__slider').forEach((item, i) => {
+		new Swiper(item, {
+			modules: [Navigation],
+			watchOverflow: true,
+			loop: true,
+			threshold: 10,
+			navigation: {
+				nextEl: `.team__button_next`,
+				prevEl: `.team__button_prev`,
+			},
+			breakpoints: {
+				0: {
+					spaceBetween: 16,
+					slidesPerView: 1.2
+				},
+				480: {
+					spaceBetween: 20,
+					slidesPerView: 2
+				},
+				780: {
+					spaceBetween: 40,
+					slidesPerView: 3
+				}
+			}
+		});
+	});
 
 })();
